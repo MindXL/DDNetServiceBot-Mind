eb52cf4704c9c2fadae2f8653b9553a1a8ef2549
MindXL <1634300602@qq.com>
2021-06-05 01:46:05 +0800

    update No.1

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,15 +1,15 @@
 const Config = {
     // official
-    // mysqlDB: 'koishi',
-    // selfId: '1718209151',
-    // modGroup: '1135333664',
-    // motGroup: '1135333664',
+    mysqlDB: 'koishi',
+    selfId: '1718209151',
+    modGroup: '1135333664',
+    motGroup: '1135333664',
 
     // only for develop
-    mysqlDB: '_koishi',
-    selfId: '1066974992',
-    modGroup: '834904988',
-    motGroup: '834904988',
+    // mysqlDB: '_koishi',
+    // selfId: '1066974992',
+    // modGroup: '834904988',
+    // motGroup: '834904988',
 
     mainQQ: '1634300602',
     bot2Id: '1718209151',
