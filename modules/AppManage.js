34709a9513ac2d116f72640ad91a851fa35b4559
MindXL <1634300602@qq.com>
2021-07-25 05:04:35 +0800

    removed all js files

--- modules/AppManage.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("koishi-adapter-onebot");
-module.exports.name = 'AppManage';
-module.exports.apply = function (app) { };
