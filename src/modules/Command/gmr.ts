533c83a69ea94673dc6932b9ce8d296427a8b99b
MindXL <1634300602@qq.com>
2021-08-17 14:24:58 +0800

    fixed sth doing with Promise

--- src/modules/Command/gmr.ts
+++ src/modules/Command/gmr.ts
@@ -10,52 +10,31 @@ export function gmr(ctx: Context, logger: Logger) {
         .usage('(Group Member Request)')
         .action(async ({ session }) => {
             try {
-                session?.observeChannel;
                 const gmrs = await ctx.database.getGMRByNum(5);
                 if (Array.isArray(gmrs) && gmrs.length === 0)
                     return '$所有入群申请已被处理$';
 
                 let errors: string[] = [];
-                await Promise.all(
-                    gmrs.map(async gmr => [
-                        ...(await sendGMRReminder(session?.bot as CQBot, gmr)),
-                        gmr.messageId,
-                    ])
-                ).then(results => {
-                    results.map(
-                        async ([newReplyMessageId, error, messageId]) => {
-                            if (newReplyMessageId) {
-                                await ctx.database.updateGMR(
-                                    'messageId',
-                                    { messageId: messageId! },
-                                    newReplyMessageId
-                                );
-                            } else {
-                                errors.push(error!);
-                            }
-                        }
+                // `gmrs.map()` didn't act well, there's sth doing with Promise
+                for (const gmr of gmrs) {
+                    const [newReplyMessageId, error] = await sendGMRReminder(
+                        session?.bot as CQBot,
+                        gmr
                     );
-                });
-                // gmrs.map(async gmr => {
-                //     const [newReplyMessageId, error] = await sendGMRReminder(
-                //         session?.bot as CQBot,
-                //         gmr
-                //     );
-                //     console.log(error?.slice(0, 10));
 
-                //     if (newReplyMessageId) {
-                //         await ctx.database.updateGMR(
-                //             'messageId',
-                //             { messageId: gmr.messageId },
-                //             newReplyMessageId
-                //         );
-                //     } else {
-                //         errors.push(error!);
-                //     }
-                // });
+                    if (newReplyMessageId) {
+                        await ctx.database.updateGMR(
+                            'messageId',
+                            { messageId: gmr.messageId },
+                            newReplyMessageId
+                        );
+                    } else {
+                        errors.push(error!);
+                    }
+                }
 
-                console.log(errors);
-                if (errors.length) throw new Error(errors[0]);
+                // 风控错误，无法发送消息，return nothing
+                if (errors.length) return;
             } catch (e) {
                 logger.extend('gmr').error(e);
                 return Config.UnknownErrorMsg;
