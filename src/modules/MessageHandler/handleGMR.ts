3230664b355f120937cc80a2ddd4f9230fad7a19
MindXL <1634300602@qq.com>
2021-08-26 00:09:49 +0800

    no bot reply when moderator reply is incorrectly formatted

--- src/modules/MessageHandler/handleGMR.ts
+++ src/modules/MessageHandler/handleGMR.ts
@@ -20,8 +20,14 @@ export function handleGMR(ctx: Context, logger: Logger) {
             // 若被回复的消息非提示入群申请的消息
             if (!gmr) return next();
 
-            const userId = session.userId!;
+            // 若消息格式不正确
+            const regExp =
+                /]\s*([yY]|(?:[nN]|[nN]\s*(?<reason>.*))|[iI])\s*$/g.exec(
+                    session.content!
+                );
+            if (!regExp) return next();
 
+            const userId = session.userId!;
             // 若非在册的管理员
             const { authority: modAuthority } = await ctx.database.getUser(
                 'onebot',
@@ -37,57 +43,47 @@ export function handleGMR(ctx: Context, logger: Logger) {
                         '进行注册哦！'
                 );
 
-            const regExp =
-                /]\s*([yY]|(?:[nN]|[nN]\s*(?<reason>.*))|[iI])\s*$/g.exec(
-                    session.content!
-                );
-
             let botReply: string;
-            if (regExp) {
-                const modReply = regExp![1];
-                const reason = regExp!.groups?.reason;
-
-                if (modReply.match(/[yY]/)) {
-                    botReply = '同意了该用户的入群申请';
-                    session.bot.handleGroupMemberRequest(gmr.messageId, true);
-                } else if (modReply.match(/[nN]/)) {
-                    botReply =
-                        '拒绝了该用户的入群申请\n' +
-                        '拒绝理由：\n' +
-                        (reason ?? '无');
-                    session.bot.handleGroupMemberRequest(
-                        gmr.messageId,
-                        false,
-                        reason
-                    );
-                } else {
-                    botReply = '忽略了该用户的入群申请';
-                }
-
-                await ctx.database.removeGMR('replyMessageId', {
-                    replyMessageId,
-                });
-                await session.bot.deleteMessage(
-                    session.groupId!,
-                    gmr.replyMessageId
+            const modReply = regExp[1];
+            const reason = regExp.groups!.reason;
+            if (modReply.match(/[yY]/)) {
+                botReply = '同意了该用户的入群申请';
+                session.bot.handleGroupMemberRequest(gmr.messageId, true);
+            } else if (modReply.match(/[nN]/)) {
+                botReply =
+                    '拒绝了该用户的入群申请\n' +
+                    '拒绝理由：\n' +
+                    (reason ?? '无');
+                session.bot.handleGroupMemberRequest(
+                    gmr.messageId,
+                    false,
+                    reason
                 );
-                if (gmr.extraMsgIds)
-                    for (const extraMsgId of gmr.extraMsgIds)
-                        await session.bot.deleteMessage(
-                            session.groupId!,
-                            extraMsgId
-                        );
             } else {
-                botReply = '回复的消息格式似乎不正确，请重新回复';
+                botReply = '忽略了该用户的入群申请';
             }
 
-            session.sendQueued(
+            await ctx.database.removeGMR('replyMessageId', {
+                replyMessageId,
+            });
+
+            await session.sendQueued(
                 s('quote', { id: replyMessageId }) +
                     s('at', { id: userId }) +
                     s('at', { id: userId }) +
                     `\n${botReply}`
             );
 
+            await session.bot.deleteMessage(
+                session.groupId!,
+                gmr.replyMessageId
+            );
+            if (gmr.extraMsgIds)
+                for (const extraMsgId of gmr.extraMsgIds)
+                    await session.bot.deleteMessage(
+                        session.groupId!,
+                        extraMsgId
+                    );
             // 管理员不可撤回群主和管理员的消息，这种错误不需抛出
             session.bot
                 .deleteMessage(session.groupId!, session.messageId!)
